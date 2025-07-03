963a0c4920a7e0a4214b6a20ff7079ba6cfe543c
Di-Boss <mttonchev@gmail.com>
2025-07-03 17:29:43 +0300

    add header panel with logo and title label

--- PreviousLives.UI/Form1.Designer.cs
+++ PreviousLives.UI/Form1.Designer.cs
@@ -28,23 +28,50 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.picLogo = new System.Windows.Forms.PictureBox();
+            this.panelHeader = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             this.SuspendLayout();
+            // 
+            // picLogo
+            // 
+            this.picLogo.BackColor = System.Drawing.Color.Transparent;
+            this.picLogo.Location = new System.Drawing.Point(1059, 507);
+            this.picLogo.Name = "picLogo";
+            this.picLogo.Size = new System.Drawing.Size(48, 48);
+            this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picLogo.TabIndex = 0;
+            this.picLogo.TabStop = false;
+            // 
+            // panelHeader
+            // 
+            this.panelHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelHeader.Location = new System.Drawing.Point(0, 0);
+            this.panelHeader.Name = "panelHeader";
+            this.panelHeader.Size = new System.Drawing.Size(1182, 60);
+            this.panelHeader.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1182, 653);
+            this.Controls.Add(this.panelHeader);
+            this.Controls.Add(this.picLogo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Previous Lives\n";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picLogo)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picLogo;
+        private System.Windows.Forms.Panel panelHeader;
     }
 }
 
--- PreviousLives.UI/Form1.cs
+++ PreviousLives.UI/Form1.cs
@@ -17,6 +17,51 @@ namespace PreviousLives
             InitializeComponent();
             this.BackColor = ColorTranslator.FromHtml("#121212");
 
+            var headerPanel = new Panel
+            {
+                Dock = DockStyle.Top,
+                Height = 80,  // match your v0 spec
+                BackColor = ColorTranslator.FromHtml("#121212"),
+                BorderStyle = BorderStyle.None
+            };
+            this.Controls.Add(headerPanel);
+
+            // 2) Add the logo PictureBox
+            var picLogo = new PictureBox
+            {
+                SizeMode = PictureBoxSizeMode.Zoom,
+                BackColor = Color.Transparent,
+                Image = Properties.Resources.hourglasslogo  // your resource name
+            };
+
+            // Now set Size and Location in normal statements:
+            // After creating picLogo and before adding to headerPanel:
+            picLogo.Size = new Size(32, 32);
+            picLogo.Location = new Point(20, (headerPanel.Height - picLogo.Height) / 2);
+
+
+            // Finally add to the header panel:
+            headerPanel.Controls.Add(picLogo);
+
+            // 4) Add the title label
+            var titleLabel = new Label
+            {
+                Text = "Previous Lives",
+                Font = new Font("Segoe UI", 18F, FontStyle.Bold),
+                ForeColor = Color.White,
+                AutoSize = true,
+                BackColor = Color.Transparent
+            };
+
+            // position it 10px to the right of the logo
+            titleLabel.Location = new Point(
+                picLogo.Right + 10,
+                (headerPanel.Height - titleLabel.PreferredHeight) / 2
+            );
+
+            headerPanel.Controls.Add(titleLabel);
+
+
 
         }
 
